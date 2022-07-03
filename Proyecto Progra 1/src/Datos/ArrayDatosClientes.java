cb3004b999f0d9959d96aec0d32913580fdeb0a5
juanra-1987 <99230947+juanra-1987@users.noreply.github.com>
2022-07-02 19:08:34 -0600

    Paquete Datos

---
+++ Proyecto Progra 1/src/Datos/ArrayDatosClientes.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Datos;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author PC
+ */
+public class ArrayDatosClientes {
+    
+    public static ArrayList<DatosClientes> listaClientes = new ArrayList<DatosClientes>();
+    
+}
