be689b3ca2bb180fe00953b3aa44a3f31ad99e1e
Yirgrin <99231022+Yirgrin@users.noreply.github.com>
2022-07-15 13:10:42 -0600

    Los panel de Trainers

--- Proyecto Progra 1/src/Visual/Trainers.java
+++ Proyecto Progra 1/src/Visual/Trainers.java
@@ -1,13 +1,26 @@
 package Visual;
 
+
+import FramesTrainers.PanelGenerarRutinas;
+import FramesTrainers.PanelOpciones2;
+import FramesTrainers.PanelPendientes;
+import FramesTrainers.PanelRegistrarse;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+
+
 public class Trainers extends javax.swing.JFrame {
 
     public Trainers() {
         initComponents();
+        
+        PanelOpciones2 p1 = new PanelOpciones2();
+        ShowPanel(p1);
 
         //Yir. Instrucciones para que aparezca en el centro y no se pueda ajustar la pantalla. 
         this.setLocationRelativeTo(null);
         this.setResizable(false);
+        
     }
 
     @SuppressWarnings("unchecked")
@@ -18,9 +31,10 @@ public class Trainers extends javax.swing.JFrame {
         panelmenu2 = new javax.swing.JPanel();
         btnopciones = new javax.swing.JButton();
         btnregistrar = new javax.swing.JButton();
-        btnpendientes = new javax.swing.JButton();
         btngenerar = new javax.swing.JButton();
+        btnpendientes = new javax.swing.JButton();
         btnvolver2 = new javax.swing.JButton();
+        PanelMostrar = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Entrenadores");
@@ -52,22 +66,37 @@ public class Trainers extends javax.swing.JFrame {
         btnregistrar.setText("Registrarse                     ");
         btnregistrar.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         btnregistrar.setHorizontalTextPosition(javax.swing.SwingConstants.LEADING);
-
-        btnpendientes.setBackground(new java.awt.Color(57, 57, 57));
-        btnpendientes.setFont(new java.awt.Font("Eras Medium ITC", 0, 16)); // NOI18N
-        btnpendientes.setForeground(new java.awt.Color(255, 255, 255));
-        btnpendientes.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/clipboard.png"))); // NOI18N
-        btnpendientes.setText("Generar rutinas            ");
-        btnpendientes.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
-        btnpendientes.setHorizontalTextPosition(javax.swing.SwingConstants.LEADING);
+        btnregistrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnregistrarActionPerformed(evt);
+            }
+        });
 
         btngenerar.setBackground(new java.awt.Color(57, 57, 57));
         btngenerar.setFont(new java.awt.Font("Eras Medium ITC", 0, 16)); // NOI18N
         btngenerar.setForeground(new java.awt.Color(255, 255, 255));
-        btngenerar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/scale-outline.png"))); // NOI18N
-        btngenerar.setText("Pendientes                     ");
+        btngenerar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/clipboard.png"))); // NOI18N
+        btngenerar.setText("Generar rutinas            ");
         btngenerar.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         btngenerar.setHorizontalTextPosition(javax.swing.SwingConstants.LEADING);
+        btngenerar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btngenerarActionPerformed(evt);
+            }
+        });
+
+        btnpendientes.setBackground(new java.awt.Color(57, 57, 57));
+        btnpendientes.setFont(new java.awt.Font("Eras Medium ITC", 0, 16)); // NOI18N
+        btnpendientes.setForeground(new java.awt.Color(255, 255, 255));
+        btnpendientes.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/scale-outline.png"))); // NOI18N
+        btnpendientes.setText("Pendientes                     ");
+        btnpendientes.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        btnpendientes.setHorizontalTextPosition(javax.swing.SwingConstants.LEADING);
+        btnpendientes.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnpendientesActionPerformed(evt);
+            }
+        });
 
         btnvolver2.setBackground(new java.awt.Color(57, 57, 57));
         btnvolver2.setFont(new java.awt.Font("Eras Medium ITC", 0, 16)); // NOI18N
@@ -94,8 +123,8 @@ public class Trainers extends javax.swing.JFrame {
                         .addGroup(panelmenu2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                             .addComponent(btnopciones, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(btnregistrar, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(btnpendientes, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(btngenerar, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(btngenerar, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnpendientes, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGap(0, 0, Short.MAX_VALUE))
                     .addComponent(btnvolver2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
@@ -108,12 +137,25 @@ public class Trainers extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btnregistrar, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btngenerar, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btnpendientes, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btngenerar, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btnvolver2, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(204, Short.MAX_VALUE))
+        );
+
+        PanelMostrar.setBackground(new java.awt.Color(102, 102, 102));
+
+        javax.swing.GroupLayout PanelMostrarLayout = new javax.swing.GroupLayout(PanelMostrar);
+        PanelMostrar.setLayout(PanelMostrarLayout);
+        PanelMostrarLayout.setHorizontalGroup(
+            PanelMostrarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 784, Short.MAX_VALUE)
+        );
+        PanelMostrarLayout.setVerticalGroup(
+            PanelMostrarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout panelprincipalLayout = new javax.swing.GroupLayout(panelprincipal);
@@ -122,11 +164,16 @@ public class Trainers extends javax.swing.JFrame {
             panelprincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(panelprincipalLayout.createSequentialGroup()
                 .addComponent(panelmenu2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 607, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(PanelMostrar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         panelprincipalLayout.setVerticalGroup(
             panelprincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(panelmenu2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(panelprincipalLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(PanelMostrar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -144,6 +191,7 @@ public class Trainers extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnvolver2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnvolver2ActionPerformed
+        //Yir. Instrucción para volver al loggin
         Loggin volverloggin = new Loggin();
         volverloggin.setVisible(true);
         this.dispose();
@@ -153,6 +201,21 @@ public class Trainers extends javax.swing.JFrame {
 
     }//GEN-LAST:event_btnopcionesMouseClicked
 
+    private void btnregistrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnregistrarActionPerformed
+        PanelRegistrarse p2 = new PanelRegistrarse();
+        ShowPanel(p2);
+    }//GEN-LAST:event_btnregistrarActionPerformed
+
+    private void btnpendientesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnpendientesActionPerformed
+        PanelPendientes p3 = new PanelPendientes();
+        ShowPanel(p3);
+    }//GEN-LAST:event_btnpendientesActionPerformed
+
+    private void btngenerarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btngenerarActionPerformed
+        PanelGenerarRutinas p4 = new PanelGenerarRutinas();
+        ShowPanel(p4);
+    }//GEN-LAST:event_btngenerarActionPerformed
+
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -161,7 +224,18 @@ public class Trainers extends javax.swing.JFrame {
         });
     }
 
+    private void ShowPanel(JPanel p) {
+        //Yir, para mostrar los paneles
+        p.setSize(800, 850);
+        p.setLocation(0, 0);
+        PanelMostrar.removeAll();
+        PanelMostrar.add(p, BorderLayout.CENTER);
+        PanelMostrar.revalidate();
+        PanelMostrar.repaint();
+   
+    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel PanelMostrar;
     private javax.swing.JButton btngenerar;
     private javax.swing.JButton btnopciones;
     private javax.swing.JButton btnpendientes;
